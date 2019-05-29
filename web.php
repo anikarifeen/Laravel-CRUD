b637b08c46bbca23428732aaba47bd60cd73a795
Bashir Ahmed <bashir0609@users.noreply.github.com>
2019-05-29 17:38:51 +0600

    Add files via upload

---
+++ web.php
@@ -0,0 +1,20 @@
+<?php
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/product/view', 'ProductController@addproductview');
+Route::post('/product/insert', 'ProductController@addproductinsert');
+Route::get('/product/delete/{product_id}', 'ProductController@addproductdelete');
+Route::get('/product/edit/{product_id}', 'ProductController@addproductedit');
+Route::get('/product/restore/{product_id}', 'ProductController@addproductrestore');
+Route::get('/product/permanentdelete/{product_id}', 'ProductController@addproductpermanentdelete');
+Route::post('/product/edit/update', 'ProductController@addproductupdate');
+
+//Frontend Routes
+
+
+Route::get('/', 'ForntendController@index')->name('home');
+Route::get('/product/details/{product_id}', 'ForntendController@productdetails')->name('home');
